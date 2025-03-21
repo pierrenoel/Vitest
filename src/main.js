adce559153fbdf04a09cc07b254437e2aed25d9f
Pierre <46715488+pierrenoel@users.noreply.github.com>
2025-03-21 12:12:29 +0100

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ let app = document.querySelector('#app');
 
 let api = import.meta.env.VITE_API_OPENWEATHER
 
+
 fetch(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${api}`).then(response => response.json()).then(data => {
-  console.log(data)
+ app.innerHTML = data.base
 })
